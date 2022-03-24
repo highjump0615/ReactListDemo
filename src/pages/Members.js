8776331de13cd70369a7900b6d15980cc67e75e5
highjump <lixiang901112@163.com>
2022-03-24 10:47:36 -0700

    feat: implement members page

--- src/pages/Members.js
+++ src/pages/Members.js
@@ -1,6 +1,36 @@
+import DataList from "../components/DataList/DataList";
+import {members} from "../constants/dummy-data";
+
 const Members = () => {
+
+  const tableHeader = () => (
+    <tr>
+      <th>ID</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Email</th>
+      <th>Team</th>
+    </tr>
+  );
+
+  const renderItem = ({item, index}) => (
+    <tr key={`item-${index}`}>
+      <td>{item.id}</td>
+      <td>{item.first_name}</td>
+      <td>{item.last_name}</td>
+      <td>{item.email}</td>
+      <td>{item.team.name}</td>
+    </tr>
+  );
+
   return (
-    <div></div>
+    <div className="dv-main">
+      <DataList
+        listHeaderComponent={tableHeader()}
+        renderItem={renderItem}
+        data={members}
+      />
+    </div>
   );
 };
 
